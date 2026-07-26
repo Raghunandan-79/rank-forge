cd2f3740624e489be96fde92657492c0c4c5e3f1
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-07-26 13:45:04 +0530

    Feat: Added primsa client

---
+++ packages/db/src/index.ts
@@ -0,0 +1,7 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+import PG from "pg";
+import { PrismaClient } from "../generated/prisma/client";
+
+const pool = new PG.Pool({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
+export const prismaClient = new PrismaClient({ adapter });
