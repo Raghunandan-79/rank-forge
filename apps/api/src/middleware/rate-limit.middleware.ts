2e6f777b52f532a5282bb33eee7aba243651fe3a
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-07-26 17:15:47 +0530

    Feat: Added rate limiter for signup

--- apps/api/src/middleware/rate-limit.middleware.ts
+++ apps/api/src/middleware/rate-limit.middleware.ts
@@ -1,6 +1,16 @@
 import { rateLimit } from "express-rate-limit";
 
-export const authRateLimiter = rateLimit({
+export const signupRateLimiter = rateLimit({
+  windowMs: 60 * 60 * 1000,
+  limit: 5,
+  standardHeaders: "draft-8",
+  legacyHeaders: false,
+  message: {
+    error: "Too many signup attempts. Please try again later",
+  },
+});
+
+export const loginRateLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   limit: 10,
   standardHeaders: "draft-8",
--- apps/api/src/routes/auth.routes.ts
+++ apps/api/src/routes/auth.routes.ts
@@ -6,12 +6,12 @@ import {
   signupController,
 } from "../controllers/auth.controller";
 import { authMiddleware } from "../middleware/auth.middleware";
-import { authRateLimiter } from "../middleware/rate-limit.middleware";
+import { loginRateLimiter, signupRateLimiter } from "../middleware/rate-limit.middleware";
 
 const authRouter = Router();
 
-authRouter.post("/signup", authRateLimiter, signupController);
-authRouter.post("/login", authRateLimiter, loginController);
+authRouter.post("/signup", signupRateLimiter,signupController);
+authRouter.post("/login", loginRateLimiter, loginController);
 authRouter.get("/me", authMiddleware, meController);
 authRouter.post("/logout", authMiddleware, logoutController);
 
