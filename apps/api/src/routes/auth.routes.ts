15eaf0bc7a7d851e1022adc017fb30c4f187ed0c
Raghunandan-79 <raghunandansharma1905@gmail.com>
2026-07-26 17:38:14 +0530

    Added csrf middleware

--- apps/api/src/routes/auth.routes.ts
+++ apps/api/src/routes/auth.routes.ts
@@ -6,13 +6,17 @@ import {
   signupController,
 } from "../controllers/auth.controller";
 import { authMiddleware } from "../middleware/auth.middleware";
-import { loginRateLimiter, signupRateLimiter } from "../middleware/rate-limit.middleware";
+import {
+  loginRateLimiter,
+  signupRateLimiter,
+} from "../middleware/rate-limit.middleware";
+import { csrfMiddleware } from "../middleware/csrf.middleware";
 
 const authRouter = Router();
 
-authRouter.post("/signup", signupRateLimiter,signupController);
+authRouter.post("/signup", signupRateLimiter, signupController);
 authRouter.post("/login", loginRateLimiter, loginController);
 authRouter.get("/me", authMiddleware, meController);
-authRouter.post("/logout", authMiddleware, logoutController);
+authRouter.post("/logout", authMiddleware, csrfMiddleware, logoutController);
 
 export default authRouter;
